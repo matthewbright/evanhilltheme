62a41484320e20d81fc6c5359341872353b226ac
Matthew Bright <me@matthewbright.net>
2015-05-20 21:59:57 -0400

    end of day 5-20

--- entry-content.php
+++ entry-content.php
@@ -1,6 +1,6 @@
 <section class="entry-content row">
-	<div class="col-sm-12">
+	<div class="not-col-sm-12"> <!-- Bypassing column for gallery width display -->
 		<?php the_content(); ?>
 		<div class="entry-links"><?php wp_link_pages(); ?></div>
-	</div> <!-- end .col -->
+	</div> 
 </section>
