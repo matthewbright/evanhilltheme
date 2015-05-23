66a8e1c1a678f426693e9a5cc2dda06bf07e5843
Matthew Bright <me@matthewbright.net>
2015-05-22 20:43:49 -0400

    add architects daughter font

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title><?php wp_title(' | ', true, 'right'); ?></title>
 		<?php wp_head(); ?>
+		<link href='http://fonts.googleapis.com/css?family=Architects+Daughter' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap-theme.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
