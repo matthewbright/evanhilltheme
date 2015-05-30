9220b5b1f783946043a961173cb2c00bb068b880
Matthew Bright <me@matthewbright.net>
2015-05-30 16:30:17 -0400

    first pass category archive

--- entry-summary.php
+++ entry-summary.php
@@ -1,4 +1,5 @@
 <section class="entry-summary">
 	<?php the_excerpt(); ?>
+	<h2>Test to see if this is entry-summary that displays here</h2>
 	<?php if(is_search()) { ?><div class="entry-links"><?php wp_link_pages(); ?></div><?php } ?>
 </section>
