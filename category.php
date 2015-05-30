e67c54d5e79d93a6d749cc98dc772892f267dd39
Matthew Bright <me@matthewbright.net>
2015-05-30 16:44:06 -0400

    cat column

--- category.php
+++ category.php
@@ -4,10 +4,14 @@
 		<h1 class="entry-title"><?php single_cat_title(); ?></h1>
 		<?php if ( '' != category_description() ) echo apply_filters('archive_meta', '<div class="archive-meta">' . category_description() . '</div>'); ?>
 	</header>
+	<div class="row">
+		<div class="col-med-4">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part('entry'); ?>
 	<?php endwhile; endif; ?>
 	<?php get_template_part('nav', 'below'); ?>
+		</div> <!-- col -->
+	</div> <!-- row -->
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- entry.php
+++ entry.php
@@ -4,49 +4,22 @@
 		<?php get_template_part('entry', 'header'); ?>
 		<?php get_template_part('entry', (is_archive() || is_search() ? 'summary' : 'content')); ?>
 	<?php else : ?>
-<article id="post-<?php the_ID(); ?>" class="post-full-tile">
+<article id="post-<?php the_ID(); ?>" class="post-full-tile gallery-item">
 				<?php if ( has_post_thumbnail() ) { ?>
 					<div class="post-thumbnail-box">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
 						<?php the_post_thumbnail('post-thumbnail', array( 'class'	=> "img-responsive attachment-post-thumbnail")); ?>
 						</a>
 						<div class="tile-meta">
+							<?php the_title(); ?>
 							<?php $scompany = get_post_meta( $post->ID, '_mb_show_company', true );
-						if ($scompany) { ?>
-							<p class="show-company"><?php echo $scompany; ?></p>
-					<?php } ?>
-							<?php $writer1 = get_post_meta( $post->ID, '_mb_show_writer1', true );
-							if ($writer1) { ?>
-								<p class="show-writer"><?php echo $writer1; ?></p>
-							<?php } ?>	
-							<?php $writer2 = get_post_meta( $post->ID, '_mb_show_writer2', true );
-								if ($writer2) { ?>
-									<p class="show-writer"><?php echo $writer2; ?></p>
-							<?php } ?>	
-							<?php $theatre = get_post_meta( $post->ID, '_mb_show_theatre', true );
-								if ($theatre) { ?>
-									<p class="show-theatre"><?php echo $theatre; ?></p>
-							<?php } ?>	
-							<?php $dirdes1 = get_post_meta( $post->ID, '_mb_show_dirdes1', true );
-								if ($dirdes1) { ?>
-									<p class="show-dirdes"><?php echo $dirdes1; ?></p>
-							<?php } ?>	
-							<?php $dirdes2 = get_post_meta( $post->ID, '_mb_show_dirdes2', true );
-								if ($dirdes2) { ?>
-									<p class="show-dirdes"><?php echo $dirdes2; ?></p>
-							<?php } ?>	
-							<?php $dirdes3 = get_post_meta( $post->ID, '_mb_show_dirdes3', true );
-								if ($dirdes3) { ?>
-									<p class="show-dirdes"><?php echo $dirdes3; ?></p>
-							<?php } ?>	
-							<?php $dirdes4 = get_post_meta( $post->ID, '_mb_show_dirdes4', true );
-								if ($dirdes4) { ?>
-									<p class="show-dirdes"><?php echo $dirdes4; ?></p>
-							<?php } ?>	
-							<?php $dirdes5 = get_post_meta( $post->ID, '_mb_show_dirdes5', true );
-								if ($dirdes5) { ?>
-									<p class="show-dirdes"><?php echo $dirdes5; ?></p>
-							<?php } ?>	
+							if ($scompany) { ?>
+								<p class="show-company"><?php echo $scompany; ?></p>
+							<?php } ?>
+							<?php $position = get_post_meta( $post->ID, '_mb_show_position', true );
+							if ($position) { ?>
+								<p class="show-position"><?php echo $position; ?></p>
+							<?php } ?>			
 						</div>
 					</div>
 				<?php } ?>
