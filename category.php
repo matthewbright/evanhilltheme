8c4a60b0f4f875e9bd025541b80a85b43c4a464e
Matthew Bright <me@matthewbright.net>
2015-05-30 16:46:33 -0400

    cat column 2

--- category.php
+++ category.php
@@ -5,13 +5,13 @@
 		<?php if ( '' != category_description() ) echo apply_filters('archive_meta', '<div class="archive-meta">' . category_description() . '</div>'); ?>
 	</header>
 	<div class="row">
-		<div class="col-med-4">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part('entry'); ?>
-	<?php endwhile; endif; ?>
-	<?php get_template_part('nav', 'below'); ?>
-		</div> <!-- col -->
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="col-md-4">
+				<?php get_template_part('entry'); ?>
+			</div> <!-- col -->
+		<?php endwhile; endif; ?>
 	</div> <!-- row -->
+	<?php get_template_part('nav', 'below'); ?>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
