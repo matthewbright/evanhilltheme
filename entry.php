98a436d783986e81360e980614fb653b9f29f9a2
Matthew Bright <me@matthewbright.net>
2015-05-30 16:54:43 -0400

    cat add borders format

--- entry.php
+++ entry.php
@@ -4,25 +4,25 @@
 		<?php get_template_part('entry', 'header'); ?>
 		<?php get_template_part('entry', (is_archive() || is_search() ? 'summary' : 'content')); ?>
 	<?php else : ?>
-<article id="post-<?php the_ID(); ?>" class="post-full-tile">
+<dl id="post-<?php the_ID(); ?>" class="post-full-tile">
 				<?php if ( has_post_thumbnail() ) { ?>
-					<div class="post-thumbnail-box gallery-icon">
+					<dt class="post-thumbnail-box gallery-icon">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
 						<?php the_post_thumbnail('post-thumbnail', array( 'class'	=> "img-responsive attachment-post-thumbnail")); ?>
 						</a>
-						<div class="tile-meta">
-							<?php the_title(); ?>
-							<?php $scompany = get_post_meta( $post->ID, '_mb_show_company', true );
-							if ($scompany) { ?>
-								<p class="show-company"><?php echo $scompany; ?></p>
-							<?php } ?>
-							<?php $position = get_post_meta( $post->ID, '_mb_show_position', true );
-							if ($position) { ?>
-								<p class="show-position"><?php echo $position; ?></p>
-							<?php } ?>			
-						</div>
-					</div>
+					</dt>
+					<dd class="tile-meta gallery-caption">
+						<?php the_title(); ?>
+						<?php $scompany = get_post_meta( $post->ID, '_mb_show_company', true );
+						if ($scompany) { ?>
+							<p class="show-company"><?php echo $scompany; ?></p>
+						<?php } ?>
+						<?php $position = get_post_meta( $post->ID, '_mb_show_position', true );
+						if ($position) { ?>
+							<p class="show-position"><?php echo $position; ?></p>
+						<?php } ?>			
+					</dd>
 				<?php } ?>
 				
 	<?php endif; ?>
-</article>
+</dl>
