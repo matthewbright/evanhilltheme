06f45d44c7d850115a3b59bae75324d7391f629e
Matthew Bright <me@matthewbright.net>
2015-05-30 16:59:44 -0400

    reconfigure cat divs

--- category.php
+++ category.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+<div class="row">
+<section id="content" role="main" class="col-sm-12">
 	<header class="header">
 		<h1 class="entry-title"><?php single_cat_title(); ?></h1>
 		<?php if ( '' != category_description() ) echo apply_filters('archive_meta', '<div class="archive-meta">' . category_description() . '</div>'); ?>
 	</header>
-	<div class="row">
+	<div class="gallery">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<div class="col-sm-4">
+			<div class="gallery-item">
 				<?php get_template_part('entry'); ?>
-			</div> <!-- col -->
+			</div> <!-- gallery-item -->
 		<?php endwhile; endif; ?>
-	</div> <!-- row -->
+	</div> <!-- gallery -->
 	<?php get_template_part('nav', 'below'); ?>
 </section>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
