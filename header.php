fb5dc3accd06beb4f589a80060fd871a23d6912e
Matthew Bright <me@matthewbright.net>
2015-05-30 21:35:04 -0400

    home page carousel

--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 	<body <?php body_class(); ?>>
 		<div id="wrapper" class="hfeed">
 			<?php if ( is_front_page() ) { ?>
-				<h1>Home Page</h1>
 				<header id="home-header" role="banner">
 					<section id="branding">
 						<div id="site-title"><h1><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php esc_attr_e( get_bloginfo('name'), 'evanhill' ); ?>" rel="home"><?php echo esc_html( get_bloginfo('name') ); ?></a></h1></div>
