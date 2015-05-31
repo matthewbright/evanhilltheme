c687e1a37f6d15b9a87a4feb22cf6d2fdd920045
Matthew Bright <me@matthewbright.net>
2015-05-30 21:18:25 -0400

    home page header

--- header.php
+++ header.php
@@ -23,7 +23,16 @@
 	<body <?php body_class(); ?>>
 		<div id="wrapper" class="hfeed">
 			<?php if ( is_front_page() ) { ?>
-<h1>Home Page</h1>
+				<h1>Home Page</h1>
+				<header id="home-header" role="banner">
+					<section id="branding">
+						<div id="site-title"><h1><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php esc_attr_e( get_bloginfo('name'), 'evanhill' ); ?>" rel="home"><?php echo esc_html( get_bloginfo('name') ); ?></a></h1></div>
+						<div id="site-description"><?php bloginfo('description'); ?></div>
+					</section>
+					<nav id="menu" role="navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+					</nav>
+				</header>
 			<?php } else { 
 				// Page header for all pages NOT the home page
 			?>
